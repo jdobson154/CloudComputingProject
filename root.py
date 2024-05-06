49133bd5036da8602299547379c8dea3ed8aa696
Atmcalpine <87542247+Atmcalpine@users.noreply.github.com>
2024-05-06 17:16:56 -0500

    Add files via upload

--- root.py
+++ root.py
@@ -6,16 +6,16 @@ import os
 import botocore
 import botocore.exceptions
 
-os.environ['AWS_ACCESS_KEY_ID']='aws_access_key' #!!!!!
-os.environ['AWS_SECRET_ACCESS_KEY']='aws_secret_access_key' #!!!!!
+os.environ['AWS_ACCESS_KEY_ID'] = 'AWS_Access_key'  # !!!!!
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'AWS_Secret_Key'  # !!!!!
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 logger = logging.getLogger(__name__)
 
-
 if 'AWS_ACCESS_KEY_ID' not in os.environ or 'AWS_SECRET_ACCESS_KEY' not in os.environ:
-    raise EnvironmentError("AWS credentials not found. Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables.")
+    raise EnvironmentError(
+        "AWS credentials not found. Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables.")
 
 region = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')  # Default to 'us-east-1' if region is not set
 dynamodb_resource = boto3.resource('dynamodb', region_name=region)
@@ -73,6 +73,7 @@ class DynamoDBHandler:
             return self.table
 
     def add_data(self, data):
+
         try:
             self.table.put_item(Item=data)
         except ClientError as err:
@@ -84,7 +85,7 @@ class DynamoDBHandler:
             )
             raise
 
-    
+
 def generateID():
     table = dynamodb_resource.Table('lolTable')
     response = table.scan()
@@ -92,13 +93,14 @@ def generateID():
 
     if len(tableEntries) == 0:
         return 1
-    
+
     idList = []
     for entry in tableEntries:
         idList.append(entry['id'])
-        
+
     nextID = max(idList) + 1
     return nextID
+
 
 def generateQuery(keys):
     table = dynamodb_resource.Table('lolTable')
@@ -129,9 +131,11 @@ def generateQuery(keys):
 
     return response
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/search')
 def search():
@@ -139,10 +143,10 @@ def search():
     table = dynamodb_resource.Table('lolTable')
     response = table.scan()
     tableEntries = response['Items']
-    
+
     for entry in tableEntries:
         newEntry = []
-        
+
         for i in range(len(entry)):
             newEntry.append(entry['id'])
             newEntry.append(entry['tournament'])
@@ -151,16 +155,17 @@ def search():
             newEntry.append(entry['score'])
             newEntry.append(entry['winner'])
             newEntry.append(entry['bracket'])
-        
+
         data.append(newEntry)
-        
+
     return render_template('search.html', data=data)
+
 
 @app.route('/search-submit', methods=['POST'])
 def searchSubmit():
     data = []
     keys = request.form.keys()
-    
+
     try:
         response = generateQuery(keys)
         tableEntries = response['Items']
@@ -170,13 +175,15 @@ def searchSubmit():
             data.append(newEntry)
 
         return render_template('search-submit.html', data=data)
-    
+
     except botocore.exceptions.ClientError:
         return redirect(url_for('search'))
+
 
 @app.route('/insert')
 def insert():
     return render_template('insert.html')
+
 
 @app.route('/insert-submit', methods=['POST'])
 def insertSubmit():
@@ -192,17 +199,20 @@ def insertSubmit():
 
     # Check if the winner is either t1 or t2
     if data['winner'] != data['t1'] and data['winner'] != data['t2']:
-        return "Invalid winner entered. Winner must be either Team 1 or Team 2."
+        message = "Invalid winner entered. Winner must be either Team 1 or Team 2."
+        logger.error(message)
+        return render_template('insert-submit.html', data='error', message=message)
 
     handler.add_data(data)
     return render_template('insert-submit.html', data=data)
 
+
 if __name__ == '__main__':
-    
+
     tableName = "lolTable"
     handler = DynamoDBHandler(dynamodb_resource)
-    
+
     if not handler.exists(table_name=tableName):
         handler.create_table(tableName)
-    
+
     app.run(host='0.0.0.0', port=11278)
